635dd65142a97ff23483eaf628b0e9ae4bdb4ca9
松田　遥 <hayutoverycute@gmail.com>
2025-02-15 02:14:42 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,40 @@
+
+document.write("<p>1. 変数にNumber型を代入してconsole.log()で表示させてください。</p>")
+
+var variableNumber = 1234;
+console.log(variableNumber);
+
+document.write("<p>2. 変数にString型を代入してconsole.log()で表示させてください。</p>")
+
+var variableText = "Text";
+console.log(variableText);
+
+
+document.write("<p>3. 変数にBoolean型を代入してconsole.log()で表示させてください。</p>")
+
+var variableBoolean = true;
+console.log(variableBoolean);
+
+
+document.write("<p>4. 10と20を足した結果を変数に代入してconsole.log()で表示させてください。</p>")
+
+console.log("4.10と20を足した結果は" + (10 + 20));
+
+document.write("<p>5. 20から10を引いた結果を変数に代入してconsole.log()で表示させてください。</p>")
+
+console.log("5. 20から10を引いた結果は" + (20 - 10));
+
+document.write("<p>6. 50へ2を掛けた結果を変数に代入してconsole.log()で表示させてください。</p>")
+
+console.log("6. 50へ2を掛けた結果は" + (50 * 2));
+
+document.write("<p>7. 20を2で割った結果を変数に代入してconsole.log()で表示させてください。</p>")
+
+console.log("7. 20を2で割った結果は" + (20 / 2));
+
+document.write("<p>8. 12を34を結合させた結果を変数に代入してconsole.log()で表示させてください。</p>")
+
+var result = 12 + 34;
+
+console.log("8. 12を34を結合させた結果は" + result );
+
