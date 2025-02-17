82fbcccb9e5b72321bcb061c5fc5f4a414d00d2f
松田　遥 <hayutoverycute@gmail.com>
2025-02-17 20:38:19 +0900

    2nd commit

--- script.js
+++ script.js
@@ -18,23 +18,32 @@ console.log(variableBoolean);
 
 document.write("<p>4. 10と20を足した結果を変数に代入してconsole.log()で表示させてください。</p>")
 
-console.log("4.10と20を足した結果は" + (10 + 20));
+var result = 10 + 20;
+
+console.log("10と20を足した結果は" + result );
 
 document.write("<p>5. 20から10を引いた結果を変数に代入してconsole.log()で表示させてください。</p>")
 
-console.log("5. 20から10を引いた結果は" + (20 - 10));
+var result = 20 - 10;
+
+console.log("20から10を引いた結果は" + result );
 
 document.write("<p>6. 50へ2を掛けた結果を変数に代入してconsole.log()で表示させてください。</p>")
 
-console.log("6. 50へ2を掛けた結果は" + (50 * 2));
+var result = 50 * 2;
+
+console.log("6. 50へ2を掛けた結果は" + result );
 
 document.write("<p>7. 20を2で割った結果を変数に代入してconsole.log()で表示させてください。</p>")
 
-console.log("7. 20を2で割った結果は" + (20 / 2));
+var result = 20 / 2;
+
+console.log("7. 20を2で割った結果は" + result );
 
 document.write("<p>8. 12を34を結合させた結果を変数に代入してconsole.log()で表示させてください。</p>")
 
-var result = 12 + 34;
+var result = '12' + '34';
 
 console.log("8. 12を34を結合させた結果は" + result );
+
 
